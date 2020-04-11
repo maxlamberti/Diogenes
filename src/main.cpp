841f872952d813c07cdf1880a1aa372bc8be7110
maxlamberti <maximilien.lamberti@gmail.com>
2020-04-11 15:58:34 -0500

    removes aws api from main script

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include <QApplication>
-#include <aws/core/Aws.h>
-
 #include "mainwindow.h"
 
 
 int main(int argc, char *argv[])
 {
-
-    Aws::SDKOptions options;
-    Aws::InitAPI(options);
+    // Start Application
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    Aws::ShutdownAPI(options);
 
     return a.exec();
 }
