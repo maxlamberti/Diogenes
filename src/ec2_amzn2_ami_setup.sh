ffeaf37b089365f8a86160229243568c190ed716
maxlamberti <maximilien.lamberti@gmail.com>
2020-04-12 23:57:33 -0500

    updates install scripts and adds deep learning installs

---
+++ src/ec2_amzn2_ami_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Updates and other installations 
+sudo yum update -y
+sudo yum install -y git screen htop
+
+# Install Anaconda
+wget https://repo.continuum.io/archive/Anaconda3-2020.02-Linux-x86_64.sh -O ~/anaconda.sh
+bash ~/anaconda.sh -b -p $HOME/anaconda
+rm ~/anaconda.sh
+eval "$($HOME/anaconda/bin/conda shell.bash hook)"
+conda init bash
+
+# Start persistent jupyter lab session
+screen -d -m jupyter lab --no-browser --port=5678
+
+# View connection notebook link/token by running the next line on ec2 machine:
+# jupyter notebook list
+
+# Run next line on local machine to connect
+# ssh -CNL 5678:localhost:5678 -i PATH/TO/SSH/KEY ec2-user@EC2s.PUBLIC.IP
--- src/ec2_dl_ami_setup.sh
+++ src/ec2_dl_ami_setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Updates and other installations 
+# Updates and other installations
 sudo yum update -y
 sudo yum install -y git screen htop
 
-# Install Anaconda
-wget https://repo.continuum.io/archive/Anaconda3-2020.02-Linux-x86_64.sh -O ~/anaconda.sh
-bash ~/anaconda.sh -b -p $HOME/anaconda
-rm ~/anaconda.sh
-eval "$($HOME/anaconda/bin/conda shell.bash hook)"
-conda init bash
+# Set up anaconda
+sudo ln -s /home/ec2-user/anaconda3/etc/profile.d/conda.sh /etc/profile.d/conda.sh
+echo 'conda activate base' >> $HOME/.bashrc
+
+# Remove bloat
+rm -rf /home/ec2-user/tutorials/
+rm -rf /home/ec2-user/examples/
 
 # Start persistent jupyter lab session
 screen -d -m jupyter lab --no-browser --port=5678
@@ -19,8 +20,3 @@ screen -d -m jupyter lab --no-browser --port=5678
 
 # Run next line on local machine to connect
 # ssh -CNL 5678:localhost:5678 -i PATH/TO/SSH/KEY ec2-user@EC2s.PUBLIC.IP
-
-
-
-
-
